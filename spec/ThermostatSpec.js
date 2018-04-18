397a1b361e8788bc0f99805fe7aec83270dcbb62
Jordan Brooks <jordanbrooks@MacBook-Pro-68.local>
2018-04-18 09:48:21 +0100

    add Thermostat up function and tests

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,13 +1,16 @@
 describe('Thermostat', function() {
   var thermo = new Thermostat();
 
-  beforeEach(function(){
-    thermo = new Thermostat();
-  })
-
   describe('new', function(){
     it('initialises with a default of 20 degrees', function(){
       expect(thermo.temp).toEqual(20);
+    })
+  })
+
+  describe('up', function(){
+    it('increases temp by specified amount', function(){
+      thermo.up(10);
+      expect(thermo.temp).toEqual(30);
     })
   })
 })
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,7 @@
 function Thermostat() {
   this.temp = 20;
 }
+
+Thermostat.prototype.up = function(amount) {
+  this.temp += amount;
+}
