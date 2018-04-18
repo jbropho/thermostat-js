41441354820df8496bc514c98bc44eaaa10c8778
Jordan Brooks <jordanbrooks@MacBook-Pro-68.local>
2018-04-18 14:53:37 +0100

    create UI

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,13 +3,13 @@ function Thermostat() {
   this.powerSaving = true;
 }
 
-Thermostat.prototype.max_temp  = function() {
+Thermostat.prototype.maxTemp  = function() {
   return this.powerSaving === true ? 25 : 35; 
 }
 
 Thermostat.prototype.up = function(amount) {
   this.temp += amount;
-  if (this.temp > this.max_temp()) this.temp = this.max_temp();
+  if (this.temp > this.maxTemp()) this.temp = this.maxTemp();
 };
 
 Thermostat.prototype.down = function(amount) {
@@ -22,7 +22,14 @@ Thermostat.prototype.reset = function(){
 }
 
 Thermostat.prototype.currentUsage = function(){
-  if (this.temp < 18) return 'low'
-  if (this.temp > 25) return 'high'
+  if (this.temp < 18) return 'low';
+  if (this.temp > 25) return 'high';
   return 'medium';
+}
+
+Thermostat.prototype.changeTemp = function(reqTemp) {
+  var max = this.maxTemp();
+  this.temp = reqTemp;
+  if (this.temp > max) this.temp = max;
+  if (this.temp < 10) this.temp = 10;
 }
