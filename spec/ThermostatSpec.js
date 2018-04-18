c4cb26c8d3a695f3e7e3721f2b2d3fcc32353d67
Jordan Brooks <jordanbrooks@MacBook-Pro-68.local>
2018-04-18 12:00:24 +0100

    add power saving mode with tests

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -3,29 +3,44 @@ describe('Thermostat', function() {
 
   beforeEach(function(){
     thermo = new Thermostat();
-  })
+  });
 
   describe('new', function(){
     it('initialises with a default of 20 degrees', function(){
       expect(thermo.temp).toEqual(20);
-    })
-  })
+    });
+  });
 
   describe('up', function(){
     it('increases temp by specified amount', function(){
       thermo.up(10);
       expect(thermo.temp).toEqual(30);
+    });
+
+    it('doesnt increase above maximum (power saving)', function(){
+      var powerSave = {temp: 20, powerSaving: true};
+      Object.setPrototypeOf(powerSave, Object.getPrototypeOf(thermo));
+      powerSave.up(50);
+      expect(powerSave.temp).toEqual(25);
     })
-  })
+    
+    it('doesnt increase above maximum (NOT power saving)', function(){
+      var defaultPower = {temp: 20, powerSaving: false};
+      Object.setPrototypeOf(defaultPower, Object.getPrototypeOf(thermo));
+      defaultPower.up(50);
+      expect(defaultPower.temp).toEqual(35);
+    })
+  });
 
   describe('down', function(){
     it('decreases temp by specified amount', function(){
       thermo.down(1);
       expect(thermo.temp).toEqual(19);
-    })
+    });
+
     it('doesnt decrease below the minimum temp', function(){
       thermo.down(20);
       expect(thermo.temp).toEqual(10);
-    })
-  })
-})
+    });
+  });
+});
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,12 +1,18 @@
 function Thermostat() {
   this.temp = 20;
+  this.powerSaving = false;
+}
+
+Thermostat.prototype.max_temp  = function() {
+  return this.powerSaving === true ? 25 : 35; 
 }
 
 Thermostat.prototype.up = function(amount) {
   this.temp += amount;
-}
+  if (this.temp > this.max_temp()) this.temp = this.max_temp();
+};
 
 Thermostat.prototype.down = function(amount) {
   this.temp -= amount;
   if(this.temp < 10) this.temp = 10;
-}
+};
